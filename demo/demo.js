32b30770d870cf4cb0c74b9ad6ced686ad6b70c1
Pete Vejanurug <pete.vejanurug@gmail.com>
2017-08-08 18:55:56 +0700

    updated L4 notes

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,8 @@
-hello();
-function hello() {
-    console.log('Welcome to javascript');
-}
-function hello() {
-    console.log('I love you');
-}
+var s = 'I love you'
+var t = "I love you"
+var u = `I love you`
+
+var pokemon = 5;
+var s = `You have ${pokemon} pokemons.`
+
+console.log(s);
