50971418050c2487bc2dde82e92f219dbfceb74b
Pete Vejanurug <pete.vejanurug@gmail.com>
2017-08-12 19:05:51 +0700

    updated L5 notes

--- demo/demo.js
+++ demo/demo.js
@@ -1,21 +1,15 @@
-function distance(string) {
-	var x = 0;
-	var y = 0;
-	for (var i of string) {
-		if (i == 'N') {
-			y++;
-		}
-		if (i == 'S') {
-			y--;
-		}
-		if (i == 'E') {
-			x++;
-		}
-		if (i == 'W') {
-			x--;
-		}
-	}
-	var d = Math.sqrt((x ** 2) + (y ** 2));
-	console.log(d);
-}
-distance('NNEES');
+var add = [
+    {
+        area: 'Bangruk',
+        code: '10500'
+    },
+    {
+        area: 'Dusit',
+        code: '10300'
+    },
+    {
+        area: 'SP',
+        code: '10250'
+    }
+];
+console.log(add);
