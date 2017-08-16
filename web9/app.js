4e90094055c04364dc13b7525601f209d7966abb
Pete Vejanurug <pete.vejanurug@gmail.com>
2017-08-16 19:56:38 +0700

    updated L9 notes

--- web9/app.js
+++ web9/app.js
@@ -2,16 +2,35 @@ var express = require('express');
 var app = express();
 app.listen(2000);
 
+app.get('/grade',showGrade);
+function showGrade(req, res) {
+	var s = req.query.score;
+        var g = '';
+	if ( s >= 80 ) {
+		g = 'A';
+	} else if ( s >= 70 ) {
+		g = 'B';
+	} else if ( s >= 60 ) {
+		g = 'C';
+	} else if ( s >= 50 ) {
+		g = 'D';
+	} else {
+		g = 'F';
+	}
+res.send('The Grade is: ' + g);
+}
+/*
 app.get('/final', showFinal);
 function showFinal(req, res) {
 	var price = req.query.price;
-	if ( price > 100 ) {
+	if ( price >= 100 ) {
 		var finalprice = price * 0.95;
 	} else {
 		finalprice = price;
 	}
 res.send('The Final Price is: ' + finalprice);
 }
+*/
 
 /*
 app.get('/total', showTotal);
