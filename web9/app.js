30f49b224be555b6a7f4e598f2e8d42d5e99cc0a
Pete Vejanurug <pete.vejanurug@gmail.com>
2017-08-16 19:26:48 +0700

    updated L9 notes

--- web9/app.js
+++ web9/app.js
@@ -1,3 +1,44 @@
+var express = require('express');
+var app = express();
+app.listen(2000);
+
+app.get('/final', showFinal);
+function showFinal(req, res) {
+	var price = req.query.price;
+	if ( price > 100 ) {
+		var finalprice = price * 0.95;
+	} else {
+		finalprice = price;
+	}
+res.send('The Final Price is: ' + finalprice);
+}
+
+/*
+app.get('/total', showTotal);
+function showTotal(req, res) {
+	res.send('Total is: ' + req.query.price * 1.07);
+}
+*/
+
+/*
+app.get('/', show);
+app.get('/test', test);
+app.get('/data', showData);
+
+function test(req, res) {
+	res.send( [1, 2, 5] );
+}
+
+function showData(req, res) {
+	res.send( {title: 'Biology', price: 150} );
+}
+
+function show(req, res) {
+	res.send('Welcome to Express.js');
+}
+*/
+
+/*
 var http = require('http');
 var app = http.createServer(show);
 app.listen(2000);
@@ -7,4 +48,5 @@ function show(req, res) {
 	res.write('<i>Welcome to iCoffee</i>');
 	res.end();
 }
+*/
 
