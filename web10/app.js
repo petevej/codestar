426ef29124ca9b02bfce60b0573192f468a6bf97
Pete Vejanurug <pete.vejanurug@gmail.com>
2017-08-17 20:21:22 +0700

    updated L10 notes

--- web10/app.js
+++ web10/app.js
@@ -4,7 +4,17 @@ var app = express();
 app.engine('html', ejs.renderFile);
 app.listen(2000);
 app.get('/product', showProduct);
+app.get('/staff', showStaff);
 
 function showProduct(req, res) {
-	res.render('product.html');
+	res.render('product.html', {product: coffee});
+}
+
+var coffee = ['Americano', 'Latte', 'Mocha', 'Espresso'];
+
+function showStaff(req, res) {
+	res.render('staff.html', {staff:
+		['Mark Zuckerberg', 'Bill Gates',
+		'Elon Musk', 'Sheryl Sandberg']
+	})
 }
